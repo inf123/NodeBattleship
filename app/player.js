259f7488b31bf144f94916dfc5580e2bccf3638c
inf123 <inf123@gmail.com>
2015-05-29 14:58:29 +0200

    added random placement of ships

--- app/player.js
+++ app/player.js
@@ -18,7 +18,11 @@ function Player(id) {
     this.shipGrid[i] = -1;
   }
 
-  this.createShips();
+  if(!this.createRandomShips()) {
+    // Random placement of ships failed. Use fallback layout (should rarely happen).
+    this.ships = [];
+    this.createShips();
+  }
 };
 
 /**
@@ -27,7 +31,6 @@ function Player(id) {
  * @returns {Boolean} True if hit
  */
 Player.prototype.shoot = function(gridIndex) {
-  var shipIndex;
   if(this.shipGrid[gridIndex] >= 0) {
     // Hit!
     this.ships[this.shipGrid[gridIndex]].hits++;
@@ -38,7 +41,7 @@ Player.prototype.shoot = function(gridIndex) {
     this.shots[gridIndex] = 1;
     return false;
   }
-}
+};
 
 /**
  * Get an array of sunk ships
@@ -54,11 +57,81 @@ Player.prototype.getSunkShips = function() {
   }
 
   return sunkShips;
+};
+
+/**
+ * Create ships and place them randomly in grid
+ * @returns {Boolean}
+ */
+Player.prototype.createRandomShips = function() {
+  var shipIndex;
+  
+  for(shipIndex = 0; shipIndex < Settings.ships.length; shipIndex++) {
+    ship = new Ship(Settings.ships[shipIndex]);
+  
+    if(!this.placeShipRandom(ship, shipIndex)) {
+      return false;
+    }
+
+    this.ships.push(ship);
+  }
+  
+  return true;
+};
+
+/**
+ * Try to place a ship randomly in grid without overlapping another ship.
+ * @param {Ship} ship
+ * @param {Number} shipIndex
+ * @returns {Boolean}
+ */
+Player.prototype.placeShipRandom = function(ship, shipIndex) {
+  var i, gridIndex, xMax, yMax;
+  // retry 20 times max
+  for(i = 0; i < 20; i++) {
+    ship.horizontal = Math.random() < 0.5;
+
+    xMax = ship.horizontal ? Settings.gridCols - ship.size + 1 : Settings.gridCols;
+    yMax = ship.horizontal ? Settings.gridRows : Settings.gridRows - ship.size + 1;
+
+    ship.x = Math.floor(Math.random() * xMax);
+    ship.y = Math.floor(Math.random() * yMax);
+
+    if(!this.checkShipOverlap(ship)) {
+      // success - ship does not overlap
+      // place ship array-index in shipGrid
+      gridIndex = ship.y * Settings.gridCols + ship.x;
+      for(i = 0; i < ship.size; i++) {
+        this.shipGrid[gridIndex] = shipIndex;
+        gridIndex += ship.horizontal ? 1 : Settings.gridCols;
+      }
+      return true;
+    }
+  }
+  
+  return false;
+}
+
+/**
+ * Check if a ship overlaps another ship in the grid.
+ * @param {Ship} ship
+ * @returns {Boolean} True if ship overlaps
+ */
+Player.prototype.checkShipOverlap = function(ship) {
+  var i, gridIndex = ship.y * Settings.gridCols + ship.x;
+
+  for(i = 0; i < ship.size; i++) {
+    if(this.shipGrid[gridIndex] >= 0) {
+      return true;
+    }
+    gridIndex += ship.horizontal ? 1 : Settings.gridCols;
+  }
+
+  return false;
 }
 
 /**
  * Create ships and place them in grid
- * @todo make placement random
  */
 Player.prototype.createShips = function() {
   var shipIndex, i, gridIndex, x = 1, ship;
@@ -80,6 +153,6 @@ Player.prototype.createShips = function() {
 
     x += 2;
   }
-}
+};
 
 module.exports = Player;
